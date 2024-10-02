2d04e7569c15151b5617d50f10258ceb729d5920
Mayank-Raj3 <guptamannu884@gmail.com>
2024-10-02 18:34:50 +0530

    Added createTodo api , getById learned mappings ResponseEntity

--- src/main/java/org/example/todoapispring/TodoController.java
+++ src/main/java/org/example/todoapispring/TodoController.java
@@ -1,16 +1,41 @@
 package org.example.todoapispring;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
-    private static List<Todo> todos = new ArrayList<>();
-
+    private static List<Todo> todoList = new ArrayList<>();
     public TodoController() {
-        todos = new ArrayList<>();
-        todos.add(new Todo(1,false,"Todo 1",1));
-        todos.add(new Todo(2,false,"Todo 2",2));
+        todoList = new ArrayList<>();
+        todoList.add(new Todo(1,false,"Todo 1",1));
+        todoList.add(new Todo(2,false,"Todo 2",2));
     }
+    @GetMapping()
+    public ResponseEntity<List<Todo>> getTodos() {
+        return new ResponseEntity<>(todoList, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Todo> createTodos(@RequestBody Todo newTodo) {
+        todoList.add(newTodo);
+        return new ResponseEntity<>(newTodo, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+   public ResponseEntity<Todo> getTodoById(@PathVariable int id) {
+        for (Todo todo : todoList) {
+            if (todo.getId() == id) {
+                return new ResponseEntity<>(todo, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
 }
